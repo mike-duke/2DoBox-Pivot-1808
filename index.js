5a1a342927c7c422e2a1dd1cfe9743eef023e5ce
Mike Duke <aithon@gmail.com>
2018-09-14 08:44:51 -0600

    Refactor Card constructor and createCard function to include data attributes in article HTML

--- index.js
+++ index.js
@@ -44,15 +44,15 @@ $('.card-area').on('keydown', function(event) {
 });
 
 function createCard(task) {
-  var completeValue;
+  // var completeValue;
 
-  if (task.complete === true) {
-    completeValue = 'complete';
-  } else {
-    completeValue = '';
-  }
+  // if (task.complete === true) {
+  //   completeValue = 'complete';
+  // } else {
+  //   completeValue = '';
+  // }
 
-  var newCard = `<article id="${task.key}" class="card-container ${completeValue}">
+  var newCard = `<article data-id="${task.id}" data-complete="${task.complete}" class="card-container">
                   <h2 class="title-of-card" contenteditable="true">${task.title}</h2>
                   <button class="delete-button"></button>
                   <p class="body-of-card" contenteditable="true">${task.body}</p>
@@ -76,7 +76,7 @@ function Task(title, body) {
   this.title = title,
   this.body = body,
   this.importance = 0;
-  this.key = Date.now();
+  this.id = Date.now();
   this.complete = false;
 }
 
@@ -113,13 +113,13 @@ function verifyClick(event) {
 function storeCard(task) {
   var cardString = JSON.stringify(task);
   
-  localStorage.setItem(task.key, cardString);
+  localStorage.setItem(task.id, cardString);
 }
 
 function getTask(event) {
-  var cardHTML = $(event.target).closest('.card-container');
-  var cardHTMLId = cardHTML[0].id;
-  var retrievedTask = JSON.parse(localStorage.getItem(cardHTMLId));
+  var card = event.target.parentNode
+  var cardId = card.dataset.id
+  var retrievedTask = JSON.parse(localStorage.getItem(cardId));
   
   return retrievedTask;
 }
@@ -194,16 +194,19 @@ function filterTasks() {
 }
 
 function completeTask(event) {
+  // debugger
   if (event.target.className === 'complete-button' || event.target.className === 'complete-button-style') {
     var retrievedTask = getTask(event);
 
     retrievedTask.complete = !retrievedTask.complete;
-    $(event.target).parent().toggleClass('complete');
     storeCard(retrievedTask); 
+    // $(event.target).parent().toggleClass('complete');
+    $('.card-container').remove();
+    showCompletedTasks();
   }
 }
 
-function showCompletedTasks(event) {
+function showCompletedTasks() {
   var allTasksArray = getTaskArray();
 
   $('.card-container').remove();
